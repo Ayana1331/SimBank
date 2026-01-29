8eb5906c5167569824db2705c9e49b9cdd73ae49
Ayana1331 <bekzatovaayana6@gmail.com>
2026-01-30 01:49:35 +0600

    Yandex

--- src/kg/sim_bank/delivery/Delivery.java
+++ src/kg/sim_bank/delivery/Delivery.java
@@ -4,6 +4,9 @@ public class Delivery {
 public static void main(String[] args) {
     
     System.out.println("Delivery Yandex");
+<<<<<<< HEAD
     System.out.println("Delivery Glovo");
+=======
+>>>>>>> 764d9b8 (Yandex)
 }
 }
