e4b3c6e8b62ffa91c23e3fc6ddc386ae37c3b8dc
Ayana1331 <bekzatovaayana6@gmail.com>
2026-02-01 19:13:15 +0600

    January promo Ayana.
    January promotion1
    
    January promotion2
    
    January promotion3
    
    January promotion4
    
    January promotion5
    
    January promotion6
    
    January promotion7

---
+++ src/kg/sim_bank/promoutions/JanuaryPromoutions.java
@@ -0,0 +1,16 @@
+package kg.sim_bank.promoutions;
+
+public class JanuaryPromoutions {
+public static void main(String[] args) {
+    
+    System.out.println("January ");
+    System.out.println("Jamuary123 ");
+    System.out.println("Jamuary1234 ");
+    System.out.println("Jamuary12345 ");
+    System.out.println("Jamuary123456 ");
+    System.out.println("Jamuary1234567 ");
+    System.out.println("Jamuary12345678 ");
+    System.out.println("Jamuary123456789 ");
+    System.out.println("Jamuary12345678910 ");
+}
+}
