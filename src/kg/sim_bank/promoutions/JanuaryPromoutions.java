c180ffe4fb58fa56cf70436fd9c52d642c81fa7f
Ayana1331 <bekzatovaayana6@gmail.com>
2026-02-01 20:44:27 +0600

    4
    5
    
    6

--- src/kg/sim_bank/promoutions/JanuaryPromoutions.java
+++ src/kg/sim_bank/promoutions/JanuaryPromoutions.java
@@ -16,5 +16,8 @@ public static void main(String[] args) {
     System.out.println("1");
     System.out.println("12");
     System.out.println("123");
+    System.out.println("1234");
+    System.out.println("12345");
+    System.out.println("123456");
 }
 }
